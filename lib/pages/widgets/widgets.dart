a4f9a12ec8719f67fb4047564c7b72987a4cb611
johnny <johnnynguyenwork@gmail.com>
2021-04-03 21:56:40 +1100

    created common widgets barrel file

---
+++ lib/pages/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export './error_snack_bar.dart';
+export './safe-area_top_padding.dart';
+export './bottom_nav_bar/custom_bottom_navbar.dart';
+export './side_drawer/custom_side_drawer.dart';
+export './youtube_player/youtube_player_tile.dart';
